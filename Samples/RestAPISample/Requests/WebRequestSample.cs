5d9ea5fe87ac39b61e77e665d18430179a3364f6
ShaishavB <shaishav.b@juegostudioz.com>
2023-03-29 19:39:18 +0530

    samples are added

---
+++ Samples/RestAPISample/Requests/WebRequestSample.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace com.juego.webservice.sample
+{
+    /// <summary>
+    /// A sample request to get the data from server
+    /// </summary>
+    public class WebRequestSample : WebRequest
+    {
+        internal void Send(MonoBehaviour behaviour, Action<SampleResponse> OnSuccess, Action<WebResponse> OnFailed)
+        {
+            //Set the endpoint
+            EndPoint("auto-complete");
+            //Set body parameters
+            SetData("q", "game of thr");
+            //Set headers
+            SetHeader("X-RapidAPI-Key", "0a421f45d8mshfae0f9501e8f888p1a3445jsne61999eb0936");
+            SetHeader("X-RapidAPI-Host", "imdb8.p.rapidapi.com");
+            //Send a request
+            behaviour.StartCoroutine(Post<SampleResponse>(OnSuccess, OnFailed));
+        }
+    }
+}
---
+++ Samples/RestAPISample/Response/SampleResponse.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.juego.webservice.sample
+{
+    public class SampleResponse : WebResponse
+    {
+
+    }
+}
