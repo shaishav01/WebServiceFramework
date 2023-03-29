0afd96153afcb2aa14a783c5132ec95bc14132ba
ShaishavB <shaishav.b@juegostudioz.com>
2023-03-29 19:51:45 +0530

    Added samples by updating global gitignore file

---
+++ Samples~/RestAPISample/WebRequestManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace com.juego.webservice.sample
+{
+    public class WebRequestManager : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            WebServiceConfig.Init("https://imdb8.p.rapidapi.com/");
+
+            new WebRequestSample().Send(this, (res) =>
+            {
+                Debug.Log("Success->" + res.message);
+
+            }, (error) =>
+            {
+                Debug.Log("Error->" + error.message);
+            });
+        }
+    }
+}
